3b4962111e5ad7dd40b03532a0b374777a34fb7e
Leo <leonelahunchain40@gmail.com>
2026-08-27 20:55:49 -0300

    Primer commit flota.java

---
+++ flota.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class flota {
+    public static final int CANTIDAD_DATOS= 6;
+    public static final double CANTIDAD_MINIMA = 0;
+    public static final double CAPACIDAD_TANQUE = 400;
+
+    public static void main(String[] args){
+        int[] camiones = new int[CANTIDAD_DATOS];
+        String[] patente = new String[CANTIDAD_DATOS];
+        int[] KilometrosRecorridos = new int[CANTIDAD_DATOS];
+        double[] LitrosConsumidos = new double[CANTIDAD_DATOS];
+
+        Scanner teclado = new Scanner(System.in);
+    }
+}
