8c44337bf53dd0adc48242664e0229f3ee73de9e
Leo <leonelahunchain40@gmail.com>
2026-08-27 20:57:37 -0300

    programa insertar informacion flota

--- flota.java
+++ flota.java
@@ -5,6 +5,29 @@ public class flota {
     public static final double CANTIDAD_MINIMA = 0;
     public static final double CAPACIDAD_TANQUE = 400;
 
+    public static int[] CargarInformacionFlota(int[] camiones,String[] patente,int[] KilometrosRecorridos,double[] LitrosConsumidos,Scanner teclado){
+        double aux = 0;
+
+        for(int i = 0; i < CANTIDAD_DATOS;i++){
+            System.out.println("Ingrese la patente del camion " + (i+1) + " : ");
+            patente[i] = teclado.nextLine();
+            do{
+            System.out.println("Ingrese los kilometros recorridos: ");
+            aux = teclado.nextDouble();
+            }while(aux < CANTIDAD_MINIMA);{
+                KilometrosRecorridos[i] = (int) aux;
+            }
+            do{
+            System.out.println("Ingrese los litros consumidos por el camion " + (i+1) + " : ");
+            aux = teclado.nextDouble();
+            teclado.nextLine();
+            }while(aux < CANTIDAD_MINIMA && aux < CAPACIDAD_TANQUE);{
+                LitrosConsumidos[i] = aux;
+            }
+        }
+        return camiones;
+    }
+
     public static void main(String[] args){
         int[] camiones = new int[CANTIDAD_DATOS];
         String[] patente = new String[CANTIDAD_DATOS];
@@ -12,5 +35,7 @@ public class flota {
         double[] LitrosConsumidos = new double[CANTIDAD_DATOS];
 
         Scanner teclado = new Scanner(System.in);
+
+        CargarInformacionFlota(camiones, patente, KilometrosRecorridos, LitrosConsumidos, teclado);
     }
 }
